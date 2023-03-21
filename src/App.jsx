e8e99649c1139fbe03bd273a26aedf345b333576
bill <b10902118@ntu.edu.tw>
2023-03-21 17:00:08 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  const name = 1+1000
+  return (
+    <div>
+      <h1>
+        This is the normal HTML syntax you know { name }
+      </h1>
+    </div>
+  );
+}
+
+export default App;
