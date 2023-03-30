f3219caca37188b6c3655a7ff3347da48f4ef36c
bill <b10902118@ntu.edu.tw>
2023-03-30 10:13:28 +0800

    add intro

--- src/button.jsx
+++ src/button.jsx
@@ -1,5 +1,10 @@
+import { useState } from 'react'
 export default function MyButton(props){
+  const [color, setColor] = useState('blue');
+  const clickHandler = () => {
+    setColor(prevColor => prevColor === 'blue' ? 'red' : 'blue')
+  } 
   return (
-    <button>{props.children}</button>
+    <button onClick={clickHandler} style={{backgroundColor: color}}>{props.children}</button>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router-dom";
 export default function RootLayout() {
   return (
     <div>
-      <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/app">App</Link> 
       <div>
         <Outlet />
       </div>
